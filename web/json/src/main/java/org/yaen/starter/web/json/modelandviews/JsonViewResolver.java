56a23bba4eeae558e80386a26043df9b10ae3d7c
Yaen <yaen1111@outlook.com>
2016-05-23 15:47:47 +0800

    reorg web/assembly by technical, optimize log

---
+++ web/json/src/main/java/org/yaen/starter/web/json/modelandviews/JsonViewResolver.java
@@ -0,0 +1,51 @@
+package org.yaen.starter.web.json.modelandviews;
+
+import java.util.Locale;
+
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
+import org.yaen.starter.common.util.utils.StringUtil;
+
+import com.alibaba.fastjson.support.spring.FastJsonJsonView;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * json view resolver, the object will be convert to json with this view
+ * 
+ * @author Yaen 2016年5月19日下午4:04:20
+ */
+public class JsonViewResolver implements ViewResolver, Ordered {
+
+	/** filter of all, usually is * */
+	private static final String FILTER_OF_ALL = "*";
+
+	/** order of resolver, small is higher */
+	@Getter
+	@Setter
+	private int order = Ordered.HIGHEST_PRECEDENCE;
+
+	/** the filter of the view resolver, * means all */
+	@Getter
+	@Setter
+	private String viewFilter = "json";
+
+	/**
+	 * @see org.springframework.web.servlet.ViewResolver#resolveViewName(java.lang.String, java.util.Locale)
+	 */
+	@Override
+	public View resolveViewName(String viewName, Locale locale) throws Exception {
+		if (StringUtil.equals(this.viewFilter, FILTER_OF_ALL)) {
+			// * = all
+			return new FastJsonJsonView();
+		} else if (StringUtil.equalsIgnoreCase(viewName, this.viewFilter)) {
+			return new FastJsonJsonView();
+		}
+
+		// return null for not found, will try to get next view resolver
+		return null;
+	}
+
+}
